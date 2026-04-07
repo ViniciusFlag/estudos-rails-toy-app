7855f396fa6d2b18be41d97270564f6cf0259cf3
Vinicius Bandeira <vinicius.bandeira.goncalves@gmail.com>
2026-04-07 12:17:59 -0300

    Finalizando app toy com usuarios e micropostagem e validacoes de campos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  # GET /users/1/edit
+  # # GET /users/1/edit
   def edit
   end
 
@@ -61,6 +61,11 @@ class UsersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params.expect(:id))
+
+      first_user = User.first
+      @microposts = first_user.microposts.first
+
+      # puts userFirstMicropost
     end
 
     # Only allow a list of trusted parameters through.
